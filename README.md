a18fc7a1aa37d3660157e3d548f11c189558f99d
dohyeon5626 <kodohyeon71@gmail.com>
2022-03-22 20:32:40 +0900

    create project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # woocham_TV_backend_api
+
 웃참티비 api 서버 레포지토리
---
+++ src/test/kotlin/com/woochamtv/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.woochamtv.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
